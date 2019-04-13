42d81dad93460c4ebcc9aa4afe22185ef1c0b2d5
LZH147258 <369613662@qq.com>
2019-04-13 20:36:16 +0800

    init

--- sweet-gateway/src/main/java/com/sea/gareway/config/GlobalCorsConfig.java
+++ sweet-gateway/src/main/java/com/sea/gareway/config/GlobalCorsConfig.java
@@ -13,8 +13,8 @@ public class GlobalCorsConfig {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://www.sweet.com");
-        config.addAllowedOrigin("http://192.168.1.109");
+//        config.addAllowedOrigin("http://www.sweet.com");
+        config.addAllowedOrigin("http://192.168.10.119");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
